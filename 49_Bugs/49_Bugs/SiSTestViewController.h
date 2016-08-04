a6065831ca01172a8ccfb66624432dbf2e318fc6
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-08-04 08:43:17 +0300

    no message

---
+++ 49_Bugs/49_Bugs/SiSTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  SiSTestViewController.h
+//  49_Bugs
+//
+//  Created by Stanly Shiyanovskiy on 03.08.16.
+//  Copyright © 2016 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SiSTestViewController : ViewController
+
+@end
